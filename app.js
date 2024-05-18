16c6eb3efd1921bb8cf3d7f58a488e4627b82746
naqvishah <154655607+naqvishah@users.noreply.github.com>
2024-05-18 10:52:48 +0500

    alyAN

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+var pag = document.getElementById('pag')
+var ton = document.getElementById('ton')
+var but = document.getElementById('but')
+var butt = document.getElementById('butt')
+var butto = document.getElementById('butto')
+function myFunction() {
+    pag.style.fontWeight = ('1000')
+
+}
+function meFunction() {
+    pag.style.fontStyle = ('italic')
+
+}
+function hyFunction() {
+    pag.style.fontStyle = ('')
+
+}
+
+// but.addEventListener('click',function(){
+//     but.pag = pag.
+// })
